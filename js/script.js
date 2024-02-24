52d0b3d427de6da720f04f1ff4aa2843e246302a
taiseerrashed <taiseerrashed@gmail.com>
2024-02-24 04:16:50 +0200

    famms-proj

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-let barsBtn = document.querySelector(".header-content .bars")
+let barsBtn = document.querySelector(".header-content .bars i")
 let navbar = document.querySelector(".navbar")
 
 barsBtn.addEventListener("click" , ()=> {
     navbar.classList.toggle("active")
+    barsBtn.classList.toggle('fa-times');
 })
